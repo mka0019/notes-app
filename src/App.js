b4c74b12e21225f39c99faecb4962dab28e94084
Manpreet Kaur <mka0019@gmail.com>
2020-12-11 20:28:53 -0800

    shared, app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+      return (
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+            {/* We moved the visual content to another component > MainComponent.js
+                so that this file will just have the essential container that will pull the content/files in */}
+          </div>
+        </BrowserRouter>
+      );
+  }
 }
+
 
 export default App;
---
+++ src/shared/featurecards.js
@@ -0,0 +1,18 @@
+export const FEATURES = [
+    {
+        id: 0,
+        title:"Organization",
+        name: "Organization",
+        image: "/assets/images/paper.svg",
+        featured: true,
+        description: "Easily oragnize your notes, to-dos, and all the other random stuff. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit."
+    },
+    {
+        id: 1,
+        title:"Easy Access",
+        name: "Easy Access",
+        image: "/assets/images/accessible.svg",
+        featured: true,
+        description: "Easy Acess on desktop and mobile devices. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit."
+    }
+];
